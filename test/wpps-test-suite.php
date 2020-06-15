6c09a69388a372e8db68bca617ce83fc23fa03b9
Daniel Bereza <danielbereza50@gmail.com>
2020-06-14 20:05:26 -0400

    testing updates

---
+++ test/wpps-test-suite.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Test suite for unit and integration tests
+ *
+ * Uses the SimpleTest For WordPress plugin
+ *
+ * @link http://wordpress.org/extend/plugins/simpletest-for-wordpress/
+ */
+
+
+
+
+
+
+?>
